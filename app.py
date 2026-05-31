9c80f34de1f9655d9482f44593b1a730173a2093
Van1sexboy <ezhigezhidze@gmail.com>
2026-05-31 19:12:49 +0300

    Add metrics route test

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ import time
 
 app = Flask(__name__)
 
+time_request_count = 0
+
 
 @app.get("/time")
 def get_time():
+    global time_request_count
+
+    time_request_count += 1
+
     return jsonify({"time": int(time.time())})
+
+
+@app.get("/metrics")
+def get_metrics():
+    return jsonify({"count": time_request_count})
 
 
 if __name__ == "__main__":
